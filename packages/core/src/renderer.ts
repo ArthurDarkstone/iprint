d7d0f4faa13682387d0bd03c7207dc1b04c902a5
Arthur Darkstone <arthurlli@qq.com>
2025-07-31 18:28:21 +0800

    feat: add vueuse

--- packages/core/src/renderer.ts
+++
@@ -1,4 +0,0 @@
-export function createRenderer(options) {
-  // 提供虚拟节点 渲染到页面上的功能
-  // console.log(options)
-}
--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,20 +1,20 @@
-export function isObject(value) {
-  return typeof value === 'object' && value !== null
-}
+// export function isObject(value) {
+//   return typeof value === 'object' && value !== null
+// }
 
-/**
- * 看一下值有没有变
- * @param newValue
- * @param oldValue
- */
-export function hasChanged(newValue, oldValue) {
-  return !Object.is(newValue, oldValue)
-}
+// /**
+//  * 看一下值有没有变
+//  * @param newValue
+//  * @param oldValue
+//  */
+// export function hasChanged(newValue, oldValue) {
+//   return !Object.is(newValue, oldValue)
+// }
 
-export function isFunction(value) {
-  return typeof value === 'function'
-}
+// export function isFunction(value) {
+//   return typeof value === 'function'
+// }
 
-export function isOn(key) {
-  return /^on[A-Z]/.test(key)
-}
+// export function isOn(key) {
+//   return /^on[A-Z]/.test(key)
+// }
