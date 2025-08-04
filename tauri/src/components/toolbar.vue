a8eee70d926b13bd192babd8b83a007a588e2c50
Arthur Darkstone <arthurlli@qq.com>
2025-08-04 19:31:43 +0800

    chore: remove unused code and clean up toolbar component

--- tauri/src/components/toolbar.vue
+++ tauri/src/components/toolbar.vue
@@ -6,36 +6,41 @@ const { currentStatus, setStatus } = useStatus()
 // select tools
 const tools = [
   {
-    name: 'Pointer',
+    name: 'pointer',
     icon: 'icon-[bx--pointer]',
     action: () => {
       setStatus('pointer')
     },
   },
   {
-    name: 'Rect',
+    name: 'rect',
     icon: 'icon-[subway--rectangular]',
     action: () => {
       setStatus('rect')
     },
   },
 ]
+
+function handleGithub() {
+  window.open('https://github.com/ArthurDarkstone/iprint', '_blank')
+}
 </script>
 
 <template>
-  <div class="   w-full h-12 bg-gray-800  text-white  flex items-center  select-none justify-between px-4">
-    <div class="flex items-center">
+  <div class="w-full h-12 bg-gray-800  text-white  flex items-center  select-none justify-between ">
+    <div class="flex items-center ml-4">
       <div v-for="tool in tools" :key="tool.name" class="cursor-pointer mr-4" @click="tool.action">
-        <span :class="tool.icon" />
+        <span :class="`${tool.icon} ${currentStatus === tool.name ? 'text-blue-500' : 'text-white'}`" />
       </div>
     </div>
 
-    <div class="flex-1 text-center ">
-      iPrint Editor
+    <div class=" w-20 text-center ">
+      iPrint
     </div>
 
-    <div>
-      <!-- right -->
+    <div class="mr-4">
+      <!-- github -->
+      <span class="icon-[mdi--github] text-xxl cursor-pointer" @click="handleGithub" />
     </div>
   </div>
 </template>
