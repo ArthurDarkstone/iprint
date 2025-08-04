829b826f72e733e0f91e525206e1fa18ddf016ed
Arthur Darkstone <arthurlli@qq.com>
2025-08-04 19:18:23 +0800

    feat: update ESLint configuration and add Iconify dependencies

--- tauri/src/components/toolbar.vue
+++ tauri/src/components/toolbar.vue
@@ -1,9 +1,41 @@
 <script setup lang="ts">
-// Removed unused modelValue prop
+import { useStatus } from '@/store/status'
+
+const { currentStatus, setStatus } = useStatus()
+
+// select tools
+const tools = [
+  {
+    name: 'Pointer',
+    icon: 'icon-[bx--pointer]',
+    action: () => {
+      setStatus('pointer')
+    },
+  },
+  {
+    name: 'Rect',
+    icon: 'icon-[subway--rectangular]',
+    action: () => {
+      setStatus('rect')
+    },
+  },
+]
 </script>
 
 <template>
-  <div>
-    header toolbar
+  <div class="   w-full h-12 bg-gray-800  text-white  flex items-center  select-none justify-between px-4">
+    <div class="flex items-center">
+      <div v-for="tool in tools" :key="tool.name" class="cursor-pointer mr-4" @click="tool.action">
+        <span :class="tool.icon" />
+      </div>
+    </div>
+
+    <div class="flex-1 text-center ">
+      iPrint Editor
+    </div>
+
+    <div>
+      <!-- right -->
+    </div>
   </div>
 </template>
